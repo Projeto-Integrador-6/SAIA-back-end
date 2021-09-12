cc09e262669be1617351f36ca5407fbf445da0ab
Raphaela Mendonça <raphaelalsmendonca@gmail.com>
2021-09-11 21:22:34 -0300

    Adiciona middlewares (urlencoded e json)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-const { response } = require('express');
 const express = require('express')
 
 const app = express()
+
+//parse requests of content-type: application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
+//parse requests of content-type: application/json
+app.use(express.json())
 
 const porta = 3000;
 app.listen(porta, () => {console.log(`Aplicação rodando com SUCESSO na porta ${porta}! 🚀`)});
