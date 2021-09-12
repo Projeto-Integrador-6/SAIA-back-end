0458ce898cdf30b8fbf764bc46cbb6aad08ccefa
Raphaela Mendonça <raphaelalsmendonca@gmail.com>
2021-09-11 21:19:19 -0300

    Adiciona e cria uma conexão com o banco de dados

---
+++ src/database/db.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql')
+const Dbconfig = require('../config/config.js')
+
+//cria uma conexão com o banco de dados
+const connection = mysql.createConnection({
+    host: Dbconfig.HOST,
+    user: Dbconfig.USER,
+    password: Dbconfig.PASSWORD,
+    database: Dbconfig.DATABASE
+})
+
+//abre uma conexão com o banco de dados
+connection.connect(error => {
+    if(error) {
+        throw error
+    } else {
+        console.log(`Conexão com o banco de dados feita com sucesso.`)
+    }
+})
+
+module.exports = connection
