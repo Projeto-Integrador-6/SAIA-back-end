bae7f13c02601c782439ff7d728cc29f7096e70c
Raphaela Mendonça <raphaelalsmendonca@gmail.com>
2021-09-12 10:05:08 -0300

    Adiciona método de exclusão para tipoQuestao

--- src/controllers/tipoQuestao.controller.js
+++ src/controllers/tipoQuestao.controller.js
@@ -27,8 +27,20 @@ class tipoQuestao {
 
     update(campos, idTipoQuestao, res) {
         const sql = 'UPDATE tipoQuestao SET ? WHERE idTipoQuestao = ?'
-        
+
         connection.query(sql, [campos, idTipoQuestao], (error, results) => {
+            if (error) {
+                res.status(400).json(error)
+            } else {
+                res.status(200).json(results)
+            }
+        })
+    }
+
+    delete(idTipoQuestao, res) {
+        const sql = 'DELETE FROM tipoQuestao WHERE idTipoQuestao = ?'
+
+        connection.query(sql, idTipoQuestao, (error, results) => {
             if (error) {
                 res.status(400).json(error)
             } else {
