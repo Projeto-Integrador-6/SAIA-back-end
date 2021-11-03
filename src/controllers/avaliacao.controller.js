ed6b08c09211545a8d1fa9b4418b542207db2fe6
Raphaela Mendonça <raphaelalsmendonca@gmail.com>
2021-11-03 20:32:24 -0300

    Atualiza avaliacao e adicionar relação entre avaliacao e questao na criação

--- src/controllers/avaliacao.controller.js
+++ src/controllers/avaliacao.controller.js
@@ -1,55 +1,71 @@
 const db = require('../models/index.js')
-const Avaliacao = db.avaliacao
+const Sequelize = require('../database/db');
 
-exports.create = (req, res) => {
-    const avaliacao = {
-        nome: req.body.nome,
-        descricao: req.body.descricao,
-    }
 
-    Avaliacao.create(avaliacao)
-        .then(data => {
-            res.send(data)
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || 'Some error occurred...'
+const Avaliacao = db.avaliacao;
+const Questao = db.questao_avaliacao;
+
+module.exports = {
+    async create(req, res) {
+        const {
+            nome,
+            descricao,
+            questoes
+        } = req.body
+
+        const transaction = await Sequelize.transaction();
+
+        try {
+            const newAvaliacao = await Avaliacao.create({
+                nome: nome,
+                descricao: descricao
             })
-        })
-}
 
-exports.findAll = (req, res) => {
-    Avaliacao.findAll()
-    .then(data => {
-        res.send(data)
-    })
-    .catch(err => {
-        res.status(500).send({
-            message:
-                err.message || 'Some error occurred.'
-        })
-    })
-}
+            for(let j = 0; j < questoes.length; j++) {
+                await Questao.create({
+                    questao_id: questoes[j].idQuestao,
+                    avaliacao_id: newAvaliacao.idAvaliacao
+                })
+            }
 
-exports.delete = (req, res) => {
-    const avaliacao = {
-        idAvaliacao: req.body.idAvaliacao
-    }
-  
-    Avaliacao.destroy({
-        where: {
-            idAvaliacao: req.body.idAvaliacao
+            await transaction.commit();
+
+            res.status(200).json({ sucess: "Avaliação criada com sucesso"})
+        } catch (err) {
+            transaction.rollback();
+            res.status(400).json({ error: "Ocorreu um erro."})
         }
-    })
-        .then(data => {
-            res.send(data)
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || 'Some error occurred...'
+    },
+
+    async findAll (req, res) {
+        await Avaliacao.findAll()
+            .then(data => {
+                res.send(data)
             })
+            .catch(err => {
+                res.status(500).send({
+                    message:
+                        err.message || 'Ocorreu um erro.'
+                })
+            })
+    },
+
+    async delete(req, res) {
+        await Avaliacao.destroy({
+            where: {
+                idAvaliacao: req.body.idAvaliacao
+            }
         })
+            .then(data => {
+                res.send(data)
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message:
+                        err.message || 'Ocorreu um erro.'
+                })
+            })
+
+    }
 
 }
