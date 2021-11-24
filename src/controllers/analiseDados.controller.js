887b6f2c0a8ede5ef97e3d221d37c9997d67f290
unknown <lucasf1991@hotmail.com>
2021-11-23 22:40:46 -0300

    correções em analises, null retorna 0

--- src/controllers/analiseDados.controller.js
+++ src/controllers/analiseDados.controller.js
@@ -1,4 +1,7 @@
+const { aplicacao } = require('../models/index.js')
+const db = require('../models/index.js');
 const Sequelize = require('../database/db');
+const Aplicacao = db.aplicacao;
 
 
 module.exports = {
@@ -10,29 +13,18 @@ module.exports = {
             {
                 replacements: { idAplicacao: idAplicacao }
             })
-
-            const [erroAcerto] = await Sequelize.query(`
-                SELECT distinct(questao.nome) AS name,
-                sum(resposta.resposta = alternativa.idAlternativa
-                    AND alternativa.isAlternativaCorreta = TRUE) AS Acertos,
-                sum(resposta.resposta = alternativa.idAlternativa
-                AND alternativa.isAlternativaCorreta = FALSE) AS Erros
-                FROM resposta
-                INNER JOIN questao ON resposta.idQuestao = questao.idQuestao
-                INNER JOIN alternativa ON resposta.idQuestao = alternativa.idQuestao
-                WHERE idAplicacao = :idAplicacao`
-            ,{
-                replacements: { idAplicacao: idAplicacao }
-            })
+            
             const [porcentagem] = await Sequelize.query(`select
-                aplicacao.nome as name,
-                concat(round(( sum(resposta.resposta = alternativa.isAlternativaCorreta)/count(resposta) * 100 ),2),'%') AS hitPercentage,
-                concat(round(( sum(resposta.resposta <> alternativa.isAlternativaCorreta)/count(resposta) * 100 ),2),'%') AS errorPercentage,
+                ifnull(aplicacao.nome,0) as name,
+                ifnull(concat(round(( sum(resposta.resposta = alternativa.isAlternativaCorreta)/count(resposta) * 100 ),2),'%'),0) AS hitPercentage,
+                ifnull(concat(round(( sum(resposta.resposta <> alternativa.isAlternativaCorreta)/count(resposta) * 100 ),2),'%'),0) AS errorPercentage,
                 case
-                when concat(round(( sum(resposta.resposta = alternativa.isAlternativaCorreta)/count(resposta) * 100 ),2),'%') <= 40 then "RUIM"
+                when concat(round(( sum(resposta.resposta = alternativa.isAlternativaCorreta)/count(resposta) * 100 ),2),'%') > 0 
+                and concat(round(( sum(resposta.resposta = alternativa.isAlternativaCorreta)/count(resposta) * 100 ),2),'%') <= 40 then "RUIM"
                 when concat(round(( sum(resposta.resposta = alternativa.isAlternativaCorreta)/count(resposta) * 100 ),2),'%') > 40
                 and concat(round(( sum(resposta.resposta = alternativa.isAlternativaCorreta)/count(resposta) * 100 ),2),'%') <= 80  then "BOM"
-                else "EXCELENTE"
+                when concat(round(( sum(resposta.resposta = alternativa.isAlternativaCorreta)/count(resposta) * 100 ),2),'%') > 80 then "EXCELENTE" 
+                else 0
                 end as performance
                 from resposta
                 inner join aplicacao on resposta.idAplicacao = aplicacao.idAplicacao
@@ -43,16 +35,28 @@ module.exports = {
             ,{
                 replacements: { idAplicacao: idAplicacao}
             })
+            const [erroAcerto] = await Sequelize.query(`
+                SELECT distinct ifnull(questao.nome,0) AS name,
+                ifnull(sum(resposta.resposta = alternativa.idAlternativa
+                    AND alternativa.isAlternativaCorreta = TRUE),0) AS Acertos,
+                ifnull(sum(resposta.resposta = alternativa.idAlternativa
+                AND alternativa.isAlternativaCorreta = FALSE),0) AS Erros
+                FROM resposta
+                INNER JOIN questao ON resposta.idQuestao = questao.idQuestao
+                INNER JOIN alternativa ON resposta.idQuestao = alternativa.idQuestao
+                WHERE idAplicacao = :idAplicacao`
+            ,{
+                replacements: { idAplicacao: idAplicacao }
+            })
             const [selecionaAlternativa] = await Sequelize.query(`select  
-                distinct(questao.nome) as name, 
-                sum(resposta = 0) as "Alternativa A", 
-                sum(resposta = 1) as "Alternativa B",
-                sum(resposta = 2) as "Alternativa C", 
-                sum(resposta = 3) as "Alternativa D"
+                distinct ifnull(questao.nome,0) as name, 
+                ifnull(sum(resposta = 0),0) as "Alternativa A", 
+                ifnull(sum(resposta = 1),0) as "Alternativa B",
+                ifnull(sum(resposta = 2),0) as "Alternativa C", 
+                ifnull(sum(resposta = 3),0) as "Alternativa D"
                 from resposta
                 inner join questao on resposta.idQuestao = questao.idQuestao 
-                where resposta.idAplicacao = :idAplicacao 
-                group by name`
+                where resposta.idAplicacao = :idAplicacao`
             ,{
                 replacements: { idAplicacao: idAplicacao }
             })
